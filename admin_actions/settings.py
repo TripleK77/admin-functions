a8102d72f64a0f0c20a7d62148e46ac879d646a4
TripleK77 <kaungkhantkyaw15300@gmail.com>
2024-10-25 20:47:30 +0630

    kkk

--- admin_actions/settings.py
+++ admin_actions/settings.py
@@ -116,11 +116,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+MEDIA_URL = '/media/'
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'course','static')
+    os.path.join(BASE_DIR,'course','static'),
 ]
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
