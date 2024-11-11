f395aa1ebcd01b1bbce81ab31bcf1e895bf16f68
TripleK77 <kaungkhantkyaw15300@gmail.com>
2024-11-11 22:02:41 +0630

    kkk

--- admin_actions/settings.py
+++ admin_actions/settings.py
@@ -115,16 +115,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-MEDIA_URL = '/media/'
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'course','static'),
-]
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'course', 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# MEDIA_URL = '/media/'
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR,'course','static'),
+# ]
+# STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
